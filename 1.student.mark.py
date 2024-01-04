1e339cf33a6c1cc0c49ac3cb61709a4f82b5b194
HuyLN <lynhathuy38@gmail.com>
2024-01-04 14:40:38 +0700

    Update Practical 1 - Menu System

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,3 +1,4 @@
+import os
 def NoOfStudents():
     while True:
         try:
@@ -20,19 +21,19 @@ def NoOfCourses():
         except ValueError:
             print("Invalid input. Please enter an integer.")
 
-def GetInformation(student):
+def GetInformation(student, students):
     print(f"---------INFORMATION OF STUDENT No {student+1}---------")
     student_id = input("     Enter student ID: ")
     name = input("     Enter student name: ")
     dob = input("     Enter student date of birth: ")
-    return {'id': student_id, 'name': name, 'dob': dob}
+    students.append ({'id': student_id, 'name': name, 'dob': dob})
 
 
-def GetCourseInformation(courseNo):
+def GetCourseInformation(courseNo, courses):
     print(f"---------INFORMATION OF COURSE No {courseNo+1}---------")
     course_id = input("     Enter course ID: ")
     course_name = input("     Enter course name: ")
-    return {'id': course_id, 'name': course_name}
+    courses.append({'id': course_id, 'name': course_name})
 
 def student_mark(course, students):
     marks = {}
@@ -44,13 +45,18 @@ def student_mark(course, students):
 
 def list_courses(courses):
     print("---------LIST OF COURSES---------")
+    i = 0
     for course in courses:
-        print(f"{course['id']}. {course['name']}")
+        print(f"{i+1}.{course['id']}. {course['name']}")
+        i=i+1
 
 def list_students(students):
     print("---------LIST OF STUDENTS---------")
+    i = 0
     for student in students:
-        print(f"{student['id']}. {student['name']}")
+        
+        print(f"{i+1}.{student['id']}. {student['name']}")
+        i=i+1
 
 def show_student_marks(course, students, marks):
     print(f"---------STUDENT MARKS OF {course['name']} COURSE---------")
@@ -58,23 +64,71 @@ def show_student_marks(course, students, marks):
         mark = marks.get(student['id'])
         print(f"{student['name']} ({student['id']}): {mark}")
 
-# Input number of students
-num_students = NoOfStudents()
+def display_menu():
+    print("---------MENU---------")
+    print("1. List Courses")
+    print("2. List Students")
+    print("3. Show Student Marks for a Course")
+    print("4. Add Student")
+    print("5. Add Subject")
+    print("6. Add Marks for a Student in a Course")
+    print("7. Exit")
+    choice = input("Enter your choice (1-7): ")
+    return choice
 
 # Input student information
-students =  [GetInformation(i) for i in range(num_students)]
-
-# Input number of courses
-num_courses = NoOfCourses()
+students = []
 
 # Input course information
-courses = [GetCourseInformation(i) for i in range(num_courses)]
+courses = []
 
 # Input and list student marks for each course
-for course in courses:
-    marks = student_mark(course, students)
-list_courses(courses)
-list_students(students)
-for i in (courses):
-    show_student_marks(i, students, marks)
 
+
+while True:
+    user_choice = display_menu()
+    
+    if user_choice == '1':
+        if courses == [] :
+            os.system('cls')
+            print("You haven't add any courses yet")
+        else :
+            list_courses(courses)
+    elif user_choice == '2':
+        if students == [] :
+            os.system('cls')
+            print("You haven't add any student yet")
+        else :
+            list_students(students)
+    elif user_choice == '3':
+        os.system('cls')
+        course_choice = int(input("Enter the course number to show student marks: "))
+        show_student_marks(courses[course_choice - 1], students)
+    elif user_choice == '4':
+        os.system('cls')
+        num_students = NoOfStudents()
+        new_student = [GetInformation(i, students) for i in range(num_students)]
+        print("Student added successfully!")
+    elif user_choice == '5':
+        os.system('cls')
+        num_courses = NoOfCourses()
+        new_course = [GetCourseInformation(i, courses) for i in range(num_courses)]
+        print("Subject added successfully!")
+    elif user_choice == '6':
+        os.system('cls')
+        list_courses(courses)
+        course_choice = int(input("Enter the course number to add marks for a student: "))
+        list_students(students)
+        student_choice = int(input("Enter the student number to add marks: "))
+        mark = float(input(f"Enter marks for {students[student_choice - 1]['name']} "
+                            f"({students[student_choice - 1]['id']}) in "
+                            f"{courses[course_choice - 1]['name']}: "))
+        courses[mark[students[student_choice - 1]['id']]] = mark
+        print("Marks added successfully!")
+    elif user_choice == '7':
+        os.system('cls')
+        print("Exiting program. Goodbye!")
+        break
+    else:
+        os.system('cls')
+        print("Invalid choice. Please enter a number between 1 and 7.")
